35fd562221f93c5e88e8717ed5f001f867dc4e37
Tu Nguyen <tunguyen@ip-192-168-0-101.ap-southeast-1.compute.internal>
2021-11-17 02:18:02 +0700

    update code

--- src/consumers/handleMessageTopic.ts
+++ src/consumers/handleMessageTopic.ts
@@ -2,15 +2,26 @@ import { Container } from 'typedi';
 import url from 'url';
 import { checkDataNull } from '../helpers/object';
 
+const blockingWait = function(seconds) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function() {
+      console.log('wait for %s seconds', seconds);
+      return resolve(true);
+    }, seconds * 1000);
+  });
+};
+
 export default {
   topic: 'prepareDataToNotify',
   status: true,
-  totalConsumer: 10,
+  totalConsumer: 1,
   run: async function(message) {
-    const Channel = Container.get('ChannelModel');
     return new Promise(async function(resolve, reject) {
       try {
-
+        console.log('vao', message);
+        await blockingWait(60 * 60 * 2);
+        console.log('end');
+        resolve(true);
       } catch (error) {
         reject(error);
       }
