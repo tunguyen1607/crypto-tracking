950accaedbbf546935a148d7d2371147de5a1ffe
tunt <tunguyenapt1607@gmail.com>
2021-12-17 13:56:59 +0700

    update code

--- src/sockets/routes/crypto/v1/price.ts
+++ src/sockets/routes/crypto/v1/price.ts
@@ -13,26 +13,28 @@ export default {
     let watchList = [];
     let interval = null;
     socket.on('latest', function (message) {
-      console.log(message);
-      if (message.symbols) {
-        watchList = [...message.symbols];
+      if(message.method && message.method !== 'subscribe'){
+        if (message.symbols) {
+          watchList = [...message.symbols];
+        }
+        if (interval) {
+          clearInterval(interval);
+        }
+        if (watchList.length > 0) {
+          interval = setInterval(function () {
+            watchList.forEach(async function (item) {
+              let priceObject = await getAsync(item + '_to_usdt');
+              if (priceObject) {
+                priceObject = JSON.parse(priceObject);
+                priceObject['symbol'] = item;
+              }
+              socket.emit('latest', JSON.stringify(priceObject))
+            })
+          }, 30000)
+        }
       }
-      if (interval) {
-        clearInterval(interval);
-      }
-      if (watchList.length > 0) {
-        interval = setInterval(function () {
-          watchList.forEach(async function (item) {
-            let priceObject = await getAsync(item + '_to_usdt');
-            if (priceObject) {
-              priceObject = JSON.parse(priceObject);
-              priceObject['symbol'] = item;
-            }
-            socket.emit('latest', JSON.stringify(priceObject))
-          })
-        }, 1000)
-      }
-    })
+
+    });
   },
   middlewares: [middlewares.isAuth]
 }
