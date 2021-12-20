b9b8059c703348fed81c4c7d25851b7fb86c6961
tunt <tunguyenapt1607@gmail.com>
2021-12-20 14:47:13 +0700

    update code

--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -1,22 +1,21 @@
 import { listJSFilesSync } from '../helpers/file';
 import path from 'path';
 
-export default (io) => {
-  listJSFilesSync(path.dirname(__dirname) + '/sockets/routes').then(function(files) {
-    for (let i = 0; i < files.length; i++) {
-      console.log(files[i].file);
-      let router = io.of('/' + files[i].version + '/' + files[i].module + '/'+ files[i].fileName);
-      let middlewares = require(files[i].file).default.middlewares;
-      if(Array.isArray(middlewares)){
-        middlewares.forEach(function (middleware) {
-          router.use(middleware);
-        })
-      }else {
-        router.use(middlewares);
-      }
-      router.on('connection', function (socket) {
-          require(files[i].file).default.router(socket, io);
-        });
+export default async (io) => {
+  let files = await listJSFilesSync(path.dirname(__dirname) + '/sockets/routes');
+  for (let i = 0; i < files.length; i++) {
+    console.log(files[i].file);
+    let router = io.of('/' + files[i].version + '/' + files[i].module + '/'+ files[i].fileName);
+    let middlewares:any = require(files[i].file).default.middlewares;
+    if(middlewares instanceof Array){
+      middlewares.forEach(function (middleware) {
+        router.use(middleware);
+      })
+    }else {
+      router.use(middlewares);
     }
-  });
+    router.on('connection', function (socket) {
+      require(files[i].file).default.router(socket, io);
+    });
+  }
 };
