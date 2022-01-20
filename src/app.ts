adea4dc3ac21289c7bedf03181b2c2e09587af31
tunt <tunguyenapt1607@gmail.com>
2022-01-20 14:12:05 +0700

    update code

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,4 @@
 import 'reflect-metadata'; // We need this in order to use @Decorators
-
-import config from './config';
-
-import express from 'express';
-
-import Logger from './loaders/logger';
 import EventEmitter from 'events';
 
 async function startServer() {
@@ -16,7 +10,7 @@ async function startServer() {
    * Well, at least in node 10 without babel and at the time of writing
    * So we are using good old require.
    **/
-  await require('./loaders').default({expressApp: true, rabbitmq: true, socketIO: true});
+  await require('./loaders').default({expressApp: true, rabbitmq: true, socketIO: true, worker: true});
   let emitter = new EventEmitter();
   // or 0 to turn off the limit
   emitter.setMaxListeners(0);
